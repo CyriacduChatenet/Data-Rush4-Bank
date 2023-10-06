f62e09c7d4b540c95e3e7174325f62ad3f84968a
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-10-06 15:09:57 +0200

    fix(app): refacto accuracy file

--- src/accuracy.py
+++ src/accuracy.py
@@ -6,13 +6,12 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix
 
 class Accuracy:
+    @staticmethod
     def calculateAccuracy(data_encoded):
-        # Séparer les données en variables indépendantes et dépendantes
         X = data_encoded.iloc[:, :-1].values
         y = data_encoded.iloc[:, -1].values
         
         # Diviser les données en ensembles d'entraînement et de test
-        from sklearn.model_selection import train_test_split
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
         
         # Créer un objet de régression logistique
@@ -24,5 +23,4 @@ class Accuracy:
         # Prédire les résultats sur l'ensemble de test
         y_pred = classifier.predict(X_test)
         
-        # Afficher les résultats
         print("Accuracy:", classifier.score(X_test, y_test))
